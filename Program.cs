f74256ed64032ebf637da7ef02974c32db0b71ff
Gil <marsh.skc@gmail.com>
2020-10-01 19:25:26 -0500

    finished studio and bonus missions 1 and 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Counting_Characters
 {
@@ -6,7 +7,27 @@ namespace Counting_Characters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a string to be counted: ");
+            string userString = Console.ReadLine();
+
+            //string testString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char c in userString.ToLower().ToCharArray())
+            {
+                if (!charCount.ContainsKey(c))
+                {
+                    charCount.Add(c, 1);
+                }
+                else
+                {
+                    charCount[c]++;
+                }
+            }
+            foreach (KeyValuePair<char, int> count in charCount)
+            {
+                Console.WriteLine(count.Key + ": " + count.Value);
+            }
         }
     }
 }
